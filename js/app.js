ed8813987bbff93ac2bc63a21441b86efb61f95e
CesarSullen <cesarsuarez2303@gmail.com>
2024-05-19 15:58:33 -0400

    Almostost all done

---
+++ js/app.js
@@ -0,0 +1,50 @@
+// Carousel
+const images = document.querySelectorAll(".carousel-images img");
+let currentImage = 0;
+
+function showImage(index) {
+  images.forEach((img) => img.classList.remove("active"));
+  images[index].classList.add("active");
+}
+
+function nextImage() {
+  currentImage = (currentImage + 1) % images.length;
+  showImage(currentImage);
+}
+
+function prevImage() {
+  currentImage = (currentImage - 1 + images.length) % images.length;
+  showImage(currentImage);
+}
+
+document.getElementById("prevBtn").addEventListener("click", prevImage);
+
+document.getElementById("nextBtn").addEventListener("click", nextImage);
+
+showImage(currentImage);
+
+let timer = setInterval(nextImage, 5000);
+
+document.querySelectorAll("#prevBtn, #nextBtn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    clearInterval(timer);
+    timer = setInterval(nextImage, 5000);
+  });
+});
+
+// Interception Observer
+const sections = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+sections.forEach((section) => {
+  observer.observe(section);
+});
