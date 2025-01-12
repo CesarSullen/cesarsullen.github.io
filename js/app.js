274d55d74870c9b4a194dbbab6c93e82b249973f
CesarSullen <cesarsuarez2303@gmail.com>
2025-01-11 19:27:38 -0500

    renewed

--- js/app.js
+++ js/app.js
@@ -9,20 +9,3 @@ nextBtn.onclick = () => {
 prevBtn.onclick = () => {
 	carouselImages.prepend(carouselImages.querySelector("a:last-child"));
 };
-
-// Interception Observer
-const sections = document.querySelectorAll(".fade-in");
-
-const observer = new IntersectionObserver(
-	(entries) => {
-		entries.forEach((entry) => {
-			entry.target.classList.toggle("show", entry.isIntersecting);
-		});
-	},
-	{
-		threshold: 0.1,
-	}
-);
-sections.forEach((section) => {
-	observer.observe(section);
-});
--- js/onlyfans.js
+++ js/onlyfans.js
@@ -1,13 +1,13 @@
 function playVideo() {
-  let hidenElements = document.querySelectorAll(".d-none");
-  hidenElements.forEach(function (element) {
-    element.classList.remove("d-none");
-    element.classList.add("d-flex");
-  });
+	let hidenElements = document.querySelectorAll(".d-none");
+	hidenElements.forEach(function (element) {
+		element.classList.remove("d-none");
+		element.classList.add("d-flex");
+	});
 
-  let ofConfirmation = document.querySelector(".of-confirmation");
-  ofConfirmation.classList.add("d-none");
+	let advice = document.querySelector(".advice");
+	advice.classList.add("d-none");
 
-  let packVideo = document.getElementById("packVideo");
-  packVideo.play();
+	let packVideo = document.getElementById("packVideo");
+	packVideo.play();
 }
