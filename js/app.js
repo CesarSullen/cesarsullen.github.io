6260876093192904ed63158c78139e1ab810e248
saru <tools.customary833@passfwd.com>
2025-09-04 14:09:29 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -39,3 +39,22 @@ prevBtn.onclick = () => {
 	carouselImages.prepend(carouselImages.querySelector("a:last-child"));
 	updateProjectInfo();
 };
+
+// Interception Observer
+const animatedElements = document.querySelectorAll(
+	".show-up, .show-down, .show-left, .show-right, .bounce-in, .rotate-left, .rotate-right"
+);
+
+const observer = new IntersectionObserver(
+	(entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("animated");
+				observer.unobserve(entry.target);
+			}
+		});
+	},
+	{ root: null, rootMargin: "0px", threshold: 0.2 }
+);
+
+animatedElements.forEach((el) => observer.observe(el));
